96bf821e971ab5c9c912cf01cba5618def9aa9fc
Cora Weidner <cweidner3+git@gmail.com>
2022-04-16 21:19:56 -0400

    Move path to not require additional bit in url

--- ca/entrypoint.sh
+++ ca/entrypoint.sh
@@ -54,7 +54,7 @@ function set_bool() {
 }
 
 mkdir -p /conf
-cat ./providence/setup.php-dist \
+cat ./setup.php-dist \
     |  set_var   '__CA_DB_HOST__'            "${CA_DB_HOST}"          \
     |  set_var   '__CA_DB_USER__'            "${CA_DB_USER}"          \
     |  set_var   '__CA_DB_PASSWORD__'        "${CA_DB_PASSWORD}"      \
@@ -69,7 +69,7 @@ cat ./providence/setup.php-dist \
     |  set_bool  '__CA_USE_CLEAN_URLS__'     "${CA_USE_CLEAN_URLS}"   \
     |  set_bool  '__CA_ALLOW_INSTALLER_TO_OVERWRITE_EXISTING_INSTALLS__'  "${CA_ALLOW_INSTALLER_TO_OVERWRITE_EXISTING_INSTALLS}"  \
     |  set_bool  '__CA_STACKTRACE_ON_EXCEPTION__'  "${CA_STACKTRACE_ON_EXCEPTION}" \
-    > ./providence/setup.php
+    > ./setup.php
 
 if  [[ -z ${PHP_INI_DIR} ]]; then
     echo "Error: PHP_INI_DIR is not set" >&2
@@ -88,6 +88,6 @@ memory_limit = ${APP_PHP_MEMORY_LIMIT}
 display_errors = ${PHP_DISPLAY_ERRORS}
 EOF
 
-find ./providence/media -not -user www-data -exec chown www-data:www-data {} +
+find ./media -not -user www-data -exec chown www-data:www-data {} +
 
 exec "apache2-foreground" "${@}"
